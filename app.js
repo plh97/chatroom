546f8d03edb8079f6620a0214d6750d88925b562
pengliheng <pengliheng111@gmail.com>
2017-10-18 02:03:58 +0800

    123

--- app.js
+++ app.js
@@ -383,7 +383,7 @@ io.on('connection', function (socket) {
   });
 });
 
-servers.listen(443);
+// servers.listen(443);
 server.listen(8080);
 // if (process.env.NODE_ENV !== 'production') {
 //   const config = require('./webpack.config')
