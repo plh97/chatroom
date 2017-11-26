4d99d30e528aa946f11aa070547dfdf11921e9bf
pengliheng <pengliheng111@gmail.com>
2017-11-26 21:36:23 +0800

    123

--- webpack.config.js
+++ webpack.config.js
@@ -7,68 +7,68 @@ const
 	ExtractTextPlugin = require('extract-text-webpack-plugin'),
 	CleanWebpackPlugin = require('clean-webpack-plugin');
 
-const webpackConfig = merge(process.env.NODE_ENV == 'development' ? devWebpackConfig : prodWebpackConfig, {
-	entry: {
-		'app': [
-			'./src/client/index.jsx'
+module.exports = (env)=> {
+	console.log('NODE_ENV: ', env.NODE_ENV) // 'local'
+	return merge(env.NODE_ENV == 'dev' ? devWebpackConfig : prodWebpackConfig, {
+		entry: {
+			'app': [
+				'./src/client/index.jsx'
+			],
+			vender: [
+				'mobx',
+				'react',
+				'mobx-react',
+				'react-router',
+				'moment-timezone',
+				'react-router-dom'
+			]
+		},
+		output: {
+			filename: "[name].[hash].js",
+			chunkFilename: '[name].[chunkhash].js',
+			path: path.join(__dirname, "dist"),
+			publicPath: "/",
+		},
+		module: {
+			rules: [
+				{
+					test: /(\.less|\.css)$/,
+					use: ExtractTextPlugin.extract({
+						fallback: 'style-loader',
+						use: ['css-loader', 'less-loader']
+					})
+				}, {
+					test: /\.(png|svg|jpg|gif)$/,
+					use: [
+						'file-loader'
+					]
+				}, {
+					test: /\.(js|jsx)$/,
+					exclude: /(node_module|bower_components)/,
+					loader: 'babel-loader'
+					// },{
+					//   test: /\.json$/,
+					//   loader: 'json-loader'
+				}, {
+					test: /\.(woff|woff2|eot|ttf|otf)$/,
+					use: [
+						'file-loader'
+					]
+				}
+			]
+		},
+		plugins: [
+			new CleanWebpackPlugin(['dist']),
+			new webpack.optimize.CommonsChunkPlugin({
+				name: "vender",
+				minChunks: function (module) {
+					return module.context && module.context.indexOf("node_modules") !== -1;
+				},
+				minChunks: Infinity,
+			}),
+			new ExtractTextPlugin({
+				filename: 'index.[hash].css'
+			})
 		],
-		vender: [
-			'mobx',
-			'react',
-			'mobx-react',
-			'react-router',
-			'moment-timezone',
-			'react-router-dom'
-		]
-	},
-	output: {
-		filename: "[name].[hash].js",
-		chunkFilename: '[name].[chunkhash].js',
-		path: path.join(__dirname, "dist"),
-		publicPath: "/",
-	},
-	module: {
-		rules: [
-			{
-				test: /(\.less|\.css)$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: ['css-loader', 'less-loader']
-				})
-			}, {
-				test: /\.(png|svg|jpg|gif)$/,
-				use: [
-					'file-loader'
-				]
-			}, {
-				test: /\.(js|jsx)$/,
-				exclude: /(node_module|bower_components)/,
-				loader: 'babel-loader'
-				// },{
-				//   test: /\.json$/,
-				//   loader: 'json-loader'
-			}, {
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				use: [
-					'file-loader'
-				]
-			}
-		]
-	},
-	plugins: [
-		new CleanWebpackPlugin(['dist']),
-		new webpack.optimize.CommonsChunkPlugin({
-			name: "vender",
-			minChunks: function (module) {
-				return module.context && module.context.indexOf("node_modules") !== -1;
-			},
-			minChunks: Infinity,
-		}),
-		new ExtractTextPlugin({
-			filename: 'index.[hash].css'
-		})
-	],
-})
-
-
-module.exports = webpackConfig;
+	})
+}
