9d4fec0d8a8563b889152b0c3b988faeaeb41130
pengliheng <pengliheng111@gmail.com>
2017-11-27 21:51:47 +0800

    to Compatible ie

--- src/client/components/AsyncApp.jsx
+++ src/client/components/AsyncApp.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Sider from "../components/Sider.jsx"
 import Header from "../components/Header.jsx"
 import UserDetails from "../components/UserDetails.jsx"
@@ -13,17 +13,17 @@ import {
 	Link
 } from 'react-router-dom'
 
-const AsyncApp = ({match})=>(
-  <Trigger className="container">
-    <Sider match={match} />
-    <Layout style={{ marginLeft: 200 }} className="body">
-      <Header/>
-      <Route exact path={match.url} render={() => (
-        <h1>Please select a group.</h1>
-      )} />
-      <Route path={`${match.url}/:group_name`} component={Content} />
-    </Layout>
-    <UserDetails/>
-  </Trigger>
+const AsyncApp = ({ match }) => (
+	<Trigger className="container">
+		<Sider match={match} />
+		<Layout style={{ marginLeft: 200 }} className="body">
+			<Header />
+			<Route exact path={match.url} render={() => (
+				<h1>Please select a group.</h1>
+			)} />
+			<Route path={`${match.url}/:group_name`} component={Content} />
+		</Layout>
+		<UserDetails />
+	</Trigger>
 )
 export default AsyncApp
