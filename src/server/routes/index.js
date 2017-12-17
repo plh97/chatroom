40b1d1cb4b213e0b42748248794ba85028af22c8
pengliheng <pengliheng@gmail.com>
2017-12-17 12:52:23 +0800

    123

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,7 +1,9 @@
 const Auth = require('./auth.js')
+const Test = require('./test.js')
 var debug = require('debug')('koa-router');
 const router = require('koa-router')()
 
 router
       .get('/auth', Auth.getCode)
+      .get('/test', Test.getCode)
 module.exports = router;
