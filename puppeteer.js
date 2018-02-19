d04a70d6e82ec4d1278df46d9dc9ca785c63f6ce
pengliheng <pengliheng111@gmail.com>
2018-02-19 14:28:28 +0800

    123

--- puppeteer.js
+++ puppeteer.js
@@ -1,8 +1,8 @@
 const puppeteer = require('puppeteer');
 (async () => {
-		console.log('do something before')
-		await setTimeout(() => {
-			console.log(123)
-		}, 2000);
-		console.log('do something after')
+	console.log('do something before')
+	await setTimeout(() => {
+		console.log(123)
+	}, 2000);
+	console.log('do something after')
 })();
