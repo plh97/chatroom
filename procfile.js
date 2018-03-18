f8b27b754c737a512dd2fea9e6bf662b3d966b4f
pengliheng <pengliheng111@gmail.com>
2018-03-18 18:06:55 +0800

    123

--- procfile.js
+++ procfile.js
@@ -6,7 +6,6 @@ module.exports = (pandora) => {
     .env({
       NODE_ENV: 'prod',
       PORT: '8002',
-      ACCESS_TOKE: '15c0e717d59205c5a23cd9deed51db2b6f051f3f',
     })
     .entry('./src/server/index.js');
 
