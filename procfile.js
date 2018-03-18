8a74fca20ea637958270df834897ee6ea488420d
pengliheng <pengliheng111@gmail.com>
2018-03-18 16:53:32 +0800

    123

--- procfile.js
+++ procfile.js
@@ -7,27 +7,10 @@ module.exports = (pandora) => {
       PROD: '8002',
       NODE_ENV: 'prod',
     })
+    .config({
+      port: 8002,
+    })
     .entry('./src/server/index.js');
-
-  /**
-  * you can custom workers scale number
-  */
-  // pandora
-  //   .process('worker')
-  //   .scale(2); // .scale('auto') means os.cpus().length
-
-  /**
-   * you can also use fork mode to start application
-   */
-  // pandora
-  //   .fork('init', './src/server/index.js');
-
-  /**
-   * you can create another process here
-   */
-  // pandora
-  //   .process('background')
-  //   .nodeArgs(['--expose-gc']);
 
   /**
    * more features please visit our document.
