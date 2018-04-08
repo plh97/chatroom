38c40c91f3b136dd19642d8cf51d032f3d72d1d0
pengliheng <pengliheng111@gmail.com>
2018-04-08 19:15:36 +0800

    cors

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const WorkboxPlugin = require('workbox-webpack-plugin');
-const	CleanWebpackPlugin = require('clean-webpack-plugin');
+const	Clean = require('clean-webpack-plugin');
 
 // local
 const devWebpackConfig = require('./build/webpack.dev');
@@ -50,7 +50,7 @@ module.exports = env => merge(env.NODE_ENV === 'dev' ? devWebpackConfig : prodWe
   },
   resolve: { extensions: ['.js', '.jsx'] },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new Clean(['dist']),
     new ExtractTextPlugin({
       filename: 'index.[hash].css',
     }),
