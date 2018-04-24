e321e7c18a75e610f186c36e96b72fec486d7b71
pengliheng <pengliheng111@gmail.com>
2018-04-24 13:05:40 +0800

    wannan to sync local && online

---
+++ dist/precache-manifest.26bf7ad02aa8e76aacbb73c69e4f3b5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "url": "/vender.d9170b90833c0f4b2b78.js"
+  },
+  {
+    "revision": "226e1b878170c165e061c255ea1fd986",
+    "url": "/index.html"
+  },
+  {
+    "url": "/index.d9170b90833c0f4b2b78.css"
+  },
+  {
+    "revision": "ca1ef68de99bb1c21b54a2de9c2f5603",
+    "url": "/favicon.png"
+  },
+  {
+    "url": "/ed20bba30e23ea60213e9fe13281c10f.woff"
+  },
+  {
+    "url": "/app.d9170b90833c0f4b2b78.js"
+  }
+];
