4258d5e1dbed3a78d9681773938dfd849afe6000
pengliheng <pengliheng111@gmail.com>
2021-01-04 20:59:18 +0800

    dip(frontend): remove useless code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scoped.css';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <App />,
+  <span className="red">
+    <App />
+  </span>,
   document.getElementById('root')
 );
 
