16bb97e5431e969411db05eea1de5762d3ca0e07
plh2 <pengliheng111@gmail.com>
2023-01-05 21:37:35 +0800

    feat: modify snapshot id

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "vultr_instance" "instance" {
   plan              = "vc2-1c-1gb"
   region            = "sgp"
-  snapshot_id       = "74749aa6-d134-401f-aae7-bdda73349cb1"
+  snapshot_id       = "adff3e5e-a242-4302-980a-053714209e32"
   firewall_group_id = "dd776525-5e19-42e1-b55a-ad6da1cf6a4b"
   hostname          = "vultr.guest"
   label             = "test-label"
