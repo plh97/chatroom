7f8aa59597eb2243c02bbb5413486426ba68e2f0
plh2 <pengliheng111@gmail.com>
2023-01-05 13:44:59 +0800

    fix: deploy process

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "vultr_instance" "instance" {
   plan              = "vc2-1c-1gb"
   region            = "sgp"
-  snapshot_id       = "57beb9c5-2db3-46a2-b9ed-bfc325a4c614"
+  snapshot_id       = "74749aa6-d134-401f-aae7-bdda73349cb1"
   firewall_group_id = "dd776525-5e19-42e1-b55a-ad6da1cf6a4b"
   hostname          = "vultr.guest"
   label             = "test-label"
--- terraform/ubuntu22.sh
+++ terraform/ubuntu22.sh
@@ -20,3 +20,33 @@ echo 'y' | apt-get install zsh
 rm -rf /root/.oh-my-zsh
 echo 'y' | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s $(which zsh)
+
+# add 2 zsh plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# setup zsh config
+
+echo 'alias public_ip="dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com"
+
+export ZSH="$HOME/.oh-my-zsh"
+
+ZSH_THEME="bira"
+
+plugins=(
+  ansible
+  docker
+  emoji
+  git
+  git-flow
+  terraform
+  github
+  yarn
+  aws
+  npm
+  nvm
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+)
+
+source $ZSH/oh-my-zsh.sh' > ~/.zshrc
