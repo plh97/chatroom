ce27e584eca45799e4318af2ec4fab79c96890d1
plh2 <pengliheng111@gmail.com>
2023-01-06 12:46:40 +0800

    feat: circle ci file

--- packages/backend/__tests__/backend.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const backend = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(backend(), 'Hello from backend');
-console.info("backend tests passed");
