0ec983493fedf40140182b76662d74e33f55e833
plh2 <pengliheng111@gmail.com>
2023-01-09 00:46:31 +0800

    feat: change dns name

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -7,12 +7,14 @@ resource "vultr_dns_record" "chat_record" {
   domain = vultr_dns_domain.my_domain.id
   name   = "chat"
   data   = vultr_instance.instance.main_ip
+  ttl = 600
   type   = "A"
 }
 
 resource "vultr_dns_record" "api_record" {
   domain = vultr_dns_domain.my_domain.id
   name   = "api"
+  ttl = 600
   data   = vultr_instance.instance.main_ip
   type   = "A"
 }
