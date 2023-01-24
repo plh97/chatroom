5dd5d9b5ca8b20323a0102c39e4e868b02bbf81e
plh2 <pengliheng111@gmail.com>
2023-01-24 19:32:43 +0700

    fix: lighting house

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -21,7 +21,26 @@ const defaultConfig = {
   },
   plugins: [
     viteCompression(),
-    VitePWA(),
+    VitePWA({
+      // icons: [
+      //   {
+      //     src: "pwa-192x192.png",
+      //     sizes: "192x192",
+      //     type: "image/png",
+      //   },
+      //   {
+      //     src: "pwa-512x512.png",
+      //     sizes: "512x512",
+      //     type: "image/png",
+      //   },
+      //   {
+      //     src: "pwa-512x512.png",
+      //     sizes: "512x512",
+      //     type: "image/png",
+      //     purpose: "any maskable",
+      //   },
+      // ],
+    }),
     react(),
     AutoImports({
       include: [/\.*.$/],
