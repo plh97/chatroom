c297cfbfef30f2648827ebd79ff5e4fcba54fb70
plh97 <pengliheng111@gmail.com>
2023-05-28 20:18:37 +0800

    feat: replace chat1 from chat

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -5,7 +5,7 @@ resource "vultr_dns_domain" "my_domain" {
 
 resource "vultr_dns_record" "chat_record" {
   domain = vultr_dns_domain.my_domain.id
-  name   = "chat1"
+  name   = "chat"
   data   = vultr_instance.instance.main_ip
   ttl = 600
   type   = "A"
