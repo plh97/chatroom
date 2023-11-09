164de5eafa1185f1e1b50ac53d1154f547531e98
plh97 <pengliheng111@gmail.com>
2023-11-10 00:10:13 +0800

    feat: update work flow

---
+++ packages/frontend/src/components/RoomDialog.tsx
@@ -0,0 +1,78 @@
+import { Dispatch } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+import { USER } from "@/interfaces/IUser";
+import { RootState } from "@/store/index";
+import { Form } from "react-router-dom";
+import { addRoomThunk } from "@/store/reducer/room";
+
+type Props = Record<string, string>;
+
+interface IProps {
+  onClose: () => void;
+  isOpen: boolean;
+}
+
+export const RoomDialog = ({ isOpen, onClose }: IProps) => {
+  const myUserInfo = useSelector<RootState, Partial<USER>>((state) => {
+    return state.user.data;
+  });
+  const [roomName, setRoomName] = useState("");
+  const dispatch = useDispatch<Dispatch<any>>();
+  const navigation = useNavigate();
+  const toast = useToast();
+  async function handleAddRoom() {
+    if (!myUserInfo._id) {
+      return;
+    }
+    if (!roomName) {
+      toast({
+        title: "Warning.",
+        description: "Please input room name",
+        status: "error",
+        position: "top",
+        duration: 1000,
+      });
+      return;
+    }
+    const { payload } = await dispatch<any>(
+      addRoomThunk({
+        name: roomName,
+        member: [myUserInfo._id ?? ""],
+      })
+    );
+    onClose();
+    setRoomName("");
+    navigation(`/room/${payload._id}`);
+  }
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Create Room</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Form onSubmit={handleAddRoom}>
+            <FormControl id="roomname">
+              <FormLabel>Name: </FormLabel>
+              <Input
+                type="text"
+                autoComplete="true"
+                autoFocus
+                value={roomName}
+                onChange={(e) => setRoomName(e.target.value)}
+              />
+            </FormControl>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button mr={3} onClick={onClose}>
+            Cancel
+          </Button>
+          <Button type="button" colorScheme="blue">
+            Save
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
--- packages/frontend/src/components/Sidebar.tsx
+++ packages/frontend/src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import CSS from "csstype";
 import { useDispatch, useSelector } from "react-redux";
 import { USER } from "@/interfaces/IUser";
 import { RootState } from "@/store/index";
-import { addRoomThunk, joinRoomThunk } from "@/store/reducer/room";
+import { joinRoomThunk } from "@/store/reducer/room";
 import { logoutThunk } from "@/store/reducer/user";
 
 const style: { [key: string]: CSS.Properties } = {
@@ -18,35 +18,8 @@ export function SidebarComponent() {
     return state.user.data;
   });
   const { id = "" } = useParams();
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [roomName, setRoomName] = useState("");
   const dispatch = useDispatch<Dispatch<any>>();
   const navigation = useNavigate();
-  const toast = useToast();
-  async function handleAddRoom() {
-    if (!myUserInfo._id) {
-      return;
-    }
-    if (!roomName) {
-      toast({
-        title: "Warning.",
-        description: "Please input room name",
-        status: "error",
-        position: "top",
-        duration: 1000,
-      });
-      return;
-    }
-    const { payload } = await dispatch<any>(
-      addRoomThunk({
-        name: roomName,
-        member: [myUserInfo._id ?? ""],
-      })
-    );
-    onClose();
-    setRoomName("");
-    navigation(`/room/${payload._id}`);
-  }
   async function handleJoinDefaultRoom() {
     const { payload } = await dispatch<any>(joinRoomThunk({}));
     if (payload._id) {
@@ -56,8 +29,12 @@ export function SidebarComponent() {
   function handleLogout() {
     dispatch(logoutThunk());
   }
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <div style={style.sider} className="flex flex-col basis-72 flex-0">
+    <div
+      style={style.sider}
+      className={classnames("max-sm:hidden md:flex flex-col basis-72 flex-0")}
+    >
       <div className="flex items-center justify-center h-14">
         <Button colorScheme="grey" variant="outline" onClick={onOpen}>
           Add Room
@@ -89,33 +66,7 @@ export function SidebarComponent() {
           Logout
         </Button>
       </div>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Create Room</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <FormControl id="roomname">
-              <FormLabel>Name: </FormLabel>
-              <Input
-                type="text"
-                autoComplete="true"
-                autoFocus
-                value={roomName}
-                onChange={(e) => setRoomName(e.target.value)}
-              />
-            </FormControl>
-          </ModalBody>
-          <ModalFooter>
-            <Button mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button colorScheme="blue" onClick={handleAddRoom}>
-              Save
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      <RoomDialog isOpen={isOpen} onClose={onClose} />
     </div>
   );
 }
